f705cad9a4a9ff7baa3f35307faca40326c54bd0
Catherine Price <catprice444@gmail.com>
2020-05-23 16:01:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -60,7 +60,7 @@ class Song
     song_array = filename.split(" - ")
     song_array[1] = song_array[1].chomp(".mp3")
     song = self.new
-    song.name =   song_array[0]
+    song.name = song_array[0]
     song.artist_name = song_array[1]
   end
 
