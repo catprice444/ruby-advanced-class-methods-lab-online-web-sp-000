9afa60922d3fd8c2445af3a93dcbf9b5424e8a64
Catherine Price <catprice444@gmail.com>
2020-05-23 15:32:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,11 +19,27 @@ class Song
   def self.new_by_name(string_name)
     song = self.new
     song.name = string_name
-    song.save
+    # song.save
     song
   end
 
-  def self.create_by_name
+  # Build a class constructor `Song.create_by_name` that takes in the string name of
+  # a song and returns a song instance with that name set as its name property and
+  # the song being saved into the `@@all` class variable.
+  # 
+  # Consider:
+  # 
+  # ```ruby
+  # song = Song.create_by_name("The Middle")
+  # #=> #<Song:0x007fd2a2989ff0 @name="The Middle">
+  # song
+  # #=> #<Song:0x007fd2a2989ff0 @name="The Middle">
+  # Song.all.include?(song)
+  # #=> true
+  # ```
+
+  def self.create_by_name(string_name)
+    
   end
 
   def self.find_by_name
