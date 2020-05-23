14955a4c4ad4db68a97b459e878e19b97d215cdf
Catherine Price <catprice444@gmail.com>
2020-05-23 15:44:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,10 +32,24 @@ class Song
 
   def self.find_by_name(string_name)
     self.all.detect {|s| s.name == string_name}
-
   end
 
-  def self.find_or_create_by_name
+  # In order to prevent duplicate songs being created that actually represent the
+  # same song (based on the song name), we're going to build a
+  # `Song.find_or_create_by_name` class method. This method will accept a string
+  # name for a song and either return a matching song instance with that name or
+  # create a new song with the name and return the song instance.
+
+  def self.find_or_create_by_name(string_name)
+    no_dups = self.all.detect {|s| s.name == string_name}
+    if no_dups == nil
+      song = self.new
+      song.name = string_name
+      song.save
+      song
+    else
+      no_dups
+    end
   end
 
   def self.alphabetical
