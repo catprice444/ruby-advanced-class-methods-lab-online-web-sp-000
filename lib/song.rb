344b45b69120a548e852bd292e7c2abdd3ddbc8b
Catherine Price <catprice444@gmail.com>
2020-05-23 15:27:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,15 +16,6 @@ class Song
     song
   end
 
-
-  # Build a class constructor `Song.new_by_name` that takes in the string name of a
-  # song and returns a song instance with that name set as its name property.
-  # `Song.new_by_name` should return an instance of `Song` and not a simple string
-  # or anything else. Implement the following functionality:
-  # song = Song.new_by_name("The Middle")
-  #=> #<Song @name="The Middle">
-  # song.name #=> "The Middle"
-
   def self.new_by_name(string_name)
     song = self.new
     song.name = string_name
@@ -32,4 +23,5 @@ class Song
     song
   end
 
+  
 end
